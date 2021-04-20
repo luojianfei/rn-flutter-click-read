ebd728e1e84235914fd56d37846ad4d5a459a7f4
luo jian fei <244345776@qq.com>
2021-04-20 13:31:39 +0800

    chore:代码优化

--- android/src/main/java/com/yunti/rn/flutter/click_read/RNFlutterClickReadModule.java
+++ android/src/main/java/com/yunti/rn/flutter/click_read/RNFlutterClickReadModule.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.yunti.rn.flutter.click_read;
 
 import android.app.Activity;
 
--- android/src/main/java/com/yunti/rn/flutter/click_read/RNFlutterClickReadPackage.java
+++ android/src/main/java/com/yunti/rn/flutter/click_read/RNFlutterClickReadPackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.yunti.rn.flutter.click_read;
 
 import java.util.Arrays;
 import java.util.Collections;
